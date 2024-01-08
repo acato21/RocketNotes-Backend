d70ecb0db73cd5bcd74f7683bd56b80f6e204431
igor <souzaamorimdasilvai@gmail.com>
2024-01-08 14:45:14 -0300

    Show note created

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -3,12 +3,24 @@ const AppError = require("../utils/AppError");
 
 class NotesController{
 
+    async index(request, response){
+        
+    }
+
     async create(request, response){
 
         const { title, description, links, tags } = request.body;
         const user_id = request.user.id;
 
-        const [note_id] = await knex("notes").insert({title, description});
+        const user = await knex("users").where({id: user_id});
+
+        console.log(user);
+
+        if(!user){
+            throw new AppError("Nenhum usuário cadastrado");
+        }
+
+        const [note_id] = await knex("notes").insert({title, description, user_id});
 
         const linkInsert = links.map(link => {
             return{
@@ -41,6 +53,17 @@ class NotesController{
         await knex("notes").where(id).delete();
 
         return response.json();
+
+     }
+
+     async show(request, response){
+
+        const { note_id } = request.params;
+        const user_id = request.user.id;
+
+        const note = await knex("notes").where({id: note_id, user_id: user_id}).first();
+
+        return response.json(note);
 
      }
 }
--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -6,8 +6,10 @@ const notesController = new NotesController();
 
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
+noteRoute.post("/", ensureAuthenticated, notesController.index)
 noteRoute.post("/create", ensureAuthenticated, notesController.create);
 noteRoute.delete("/delete/:id", notesController.delete);
+noteRoute.get("/show/:note_id", ensureAuthenticated, notesController.show);
 
 
 module.exports = noteRoute;
