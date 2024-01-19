2fc158260ec68fdef4f020db65d2d8dba302e98e
igor <souzaamorimdasilvai@gmail.com>
2024-01-19 06:25:53 -0300

    Upload avatar

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -4,7 +4,46 @@ const AppError = require("../utils/AppError");
 class NotesController{
 
     async index(request, response){
+
+        const { title, tags } = request.query;
+        const user_id = request.user.id;
+
+        let note;
         
+        if(tags){
+            const Tags = tags.split(",").map(tag => tag);
+            
+            note = await knex("tags")
+            .select(
+                "notes.id",
+                "notes.title",
+                "notes.description"
+            )
+            .where("notes.user_id", user_id)
+            .whereIn("name", Tags)
+            .whereLike("notes.title", `%${title}%`)
+            .innerJoin("notes", "notes.id", "tags.note_id")
+            .orderBy("notes.title")
+        }else{
+            note = await knex("notes")
+            .whereLike("title", `%${title}%`)
+            .where(user_id)
+            orderBy("title")
+        }
+
+        const Tags = await knex("tags").where({user_id});
+
+        const insertNote = note.map(note => {
+            const TagNote = Tags.filter(tag => tag.note_id === note.id);
+
+            return{
+                ...note,
+                tags: TagNote
+            }
+        })
+        
+        return response.json(insertNote)
+
     }
 
     async create(request, response){
